24d8d85443ed53ef3151d3cdf129bf52405a1fa5
Quiddiqin <quina.chen99@gmail.com>
2023-05-16 09:18:03 +0200

    aggiunta template

---
+++ app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import warnings
+warnings.filterwarnings('ignore')
+def main():
+    st.title("Template")
+
+
+
+if __name__ == "__main__":
+    main()
+
+
+#streamlit run app.py
